fb442c2e8721da76dfb4f3a25afcd6bc48e9307e
jordles113 <jordan.t.romero@gmail.com>
2021-01-02 17:08:14 -0600

    wires up congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,9 +1,20 @@
-const Congrats = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+const Congrats = (props) => {
+    
+        if (props.success) {
+        return (
+            <div data-test='component-congrats'>
+                <span data-test='congrats-message'>
+                    'Congrats you guessed the word!'
+                </span>
+            </div>
+            )
+        } else {
+            return (
+                <div data-test='component-congrats' />
+
+            )
+        }
+    
 }
 
 export default Congrats
