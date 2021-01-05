bd5697f78b884399d39ab71f58ee607c6c1dee79
jordles113 <jordan.t.romero@gmail.com>
2021-01-05 16:55:13 -0600

    sets up propTypes in GuessedWords

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -3,9 +3,9 @@ import { shallow } from 'enzyme'
 import { findByTestAttr, checkProps } from '../test/testUtils'
 import GuessedWords from './GuessedWords'
 
-const defaultProps = [ {
+const defaultProps =  {
     guessedWords: [{guessedWord: 'train', letterMatchCount: 3}],
-}]
+}
 
 
 const setup = (props={}) => {
