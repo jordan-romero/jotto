8cb689383271eab9f048baaae801395a07a33197
jordles113 <jordan.t.romero@gmail.com>
2021-01-05 17:22:26 -0600

    finishes building out GuessedWords so far

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -2,9 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const GuessedWords = (props) => {
-    let contents =
+    let contents 
     if (props.guessedWords.length === 0) {
-        cont
+        contents = (
+            <span data-test='guess-instructions'>
+                Try to Guess the Secret Word!
+            </span> 
+        )
     }
     return (
         <div data-test='component-guessed-words'>
