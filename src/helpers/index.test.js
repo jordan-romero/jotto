2e5c6b1c2ef0f24f404bce1b956e28b33ef01653
jordles113 <jordan.t.romero@gmail.com>
2021-01-06 20:18:50 -0600

    writes test to check if letterMatchCount === 3

--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -7,9 +7,10 @@ describe ('getLetterMatchCount', () => {
         expect(letterMatchCount).toBe(0)
     }); 
     test('returns the correct count when there are 3 matching letters', () => {
-
+        const letterMatchCount =getLetterMatchCount('train', secretWord)
+        expenct(letterMatchCount).toBe(3)
     })
-    test('returns the correct count when there are duplicate letters in the guess' () => {
+    test('returns the correct count when there are duplicate letters in the guess', () => {
 
     })
 })
