348ae82b7d4cbdd316f8837f65d9a5f8db9c5c8a
jordles113 <jordan.t.romero@gmail.com>
2021-01-06 20:16:32 -0600

    writes up test for when there are no letters in common

--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -1,13 +1,15 @@
 import { getLetterMatchCount } from './'
 
 describe ('getLetterMatchCount', () => {
+    const secretWord = 'party'
     test('returns the correct count when there are no matching letters', () => {
-
+        const letterMatchCount = getLetterMatchCount('bones', secretWord)
+        expect(letterMatchCount).toBe(0)
     }); 
     test('returns the correct count when there are 3 matching letters', () => {
 
     })
     test('returns the correct count when there are duplicate letters in the guess' () => {
-        
+
     })
 })
