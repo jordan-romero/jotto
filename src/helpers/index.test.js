ed675f8193a2bfc1a67ec23304b239f3fd9cdda0
jordles113 <jordan.t.romero@gmail.com>
2021-01-06 20:13:42 -0600

    frames the three tests in index.test.js for determining letterMatchCount

--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -1,3 +1,13 @@
 import { getLetterMatchCount } from './'
 
+describe ('getLetterMatchCount', () => {
+    test('returns the correct count when there are no matching letters', () => {
 
+    }); 
+    test('returns the correct count when there are 3 matching letters', () => {
+
+    })
+    test('returns the correct count when there are duplicate letters in the guess' () => {
+        
+    })
+})
