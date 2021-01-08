4f5a4d80f43d438a4402caa56e451d7c3513103d
jordles113 <jordan.t.romero@gmail.com>
2021-01-07 19:39:46 -0600

    sets up App component and App.test"

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import GuessedWords from './GuessedWords'
 import Congrats from './Congrats'
 import './App.css'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='container'>
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='container'>
       <h1>Jotto</h1>
         <Congrats success={false}/>
         <GuessedWords guessedWords={[ {guessedWord: 'train', letterMatchCount: 3 }]}/>
-      </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default App
+
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
+import { findByTestAttr } from '../test/testUtils'
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+const setup = () => {
+    return shallow(<App />)
+}
 
 test('renders learn react link', () => {
   
