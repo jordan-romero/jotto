5633fe34da971b4efae621358f759442f927ea07
jordles113 <jordan.t.romero@gmail.com>
2021-01-08 18:41:16 -0600

    adds call to random word server

--- src/actions/hookActions.js
+++ src/actions/hookActions.js
@@ -2,7 +2,8 @@ import React from 'react'
 import axios from 'axios'
 
 export const getSecretWord = async (setSecretWord) => {
-    const response = await axios.get('')
+    const response = await axios.get('http://localhost:3030')
+    setSecretWord(response.data)
 }
 
 export default getSecretWord
