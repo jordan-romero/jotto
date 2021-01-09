bd41661e514e26882d9fccd194a478a34cb26e98
jordles113 <jordan.t.romero@gmail.com>
2021-01-08 18:33:20 -0600

    installs axios and moxios, writes moxios test

---
+++ src/actions/hookActions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios'
+
+export const getSecretWord = (setSecretWord) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default getSecretWord
---
+++ src/actions/hookActions.test.js
@@ -0,0 +1,23 @@
+import moxios from 'moxios'
+import { getSecretWord } from './hookActions'
+
+describe('moxios test', () => {
+    beforeEach(() => {
+    moxios.install()
+    })
+    afterEach(() => {
+        moxios.uninstall()
+    })
+
+    test('calls the getSecretWord callback on axios response', async () => {
+        const secretWord = 'party'
+
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent()
+            request.respondWith({
+                status: 200, 
+                response: secretWord
+            })
+        })
+    })
+})
