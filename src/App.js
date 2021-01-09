bd756133b1808121325247d0ddcc55a295113bb7
jordles113 <jordan.t.romero@gmail.com>
2021-01-09 15:23:54 -0600

    writes reducer and useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import GuessedWords from './GuessedWords'
 import Congrats from './Congrats'
 import './App.css'
 
+const reducer = (state, action) {
+  switch(action.type) {
+    case "setSecretWord":
+        return { ...state, secretWord: action.payload }
+      default: 
+        throw new Error(`Invalid action type: ${action.type}`)
+  }
+}
+
 const App = () => {
+
+  const [state, dispatch] = React.useReducer(
+    reducer, 
+    { secretWord: null }
+    )
   return (
     <div className='container' data-test='component-app'>
       <h1>Jotto</h1>
